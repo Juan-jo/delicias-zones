921592ddf57184b4a925796f27696b32d4b36c77
jdev <juanj@jdev.com>
2026-01-26 18:25:42 -0600

    config user client

--- src/main/java/org/delicias/zone_featured_partners/domain/repository/ZoneFeaturedPartnerRepository.java
+++ src/main/java/org/delicias/zone_featured_partners/domain/repository/ZoneFeaturedPartnerRepository.java
@@ -3,7 +3,15 @@ package org.delicias.zone_featured_partners.domain.repository;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.delicias.zone_featured_partners.domain.model.ZoneFeaturedPartner;
+import org.delicias.zones.domain.model.ZoneInfo;
+
+import java.util.List;
 
 @ApplicationScoped
 public class ZoneFeaturedPartnerRepository implements PanacheRepositoryBase<ZoneFeaturedPartner, Integer> {
+
+
+    public List<ZoneFeaturedPartner> findByZoneId(Integer zoneId) {
+        return list("zone.id", zoneId);
+    }
 }
